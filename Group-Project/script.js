9e45a46c27ac7af3201cb4cfb19bad18a81b6bca
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2022-10-17 20:34:38 +0700

    add delete button

--- Group-Project/script.js
+++ Group-Project/script.js
@@ -22,6 +22,31 @@ async function postMockApi(url, payload) {
   return result;
 }
 
+// putMockApi('blablabla.mock.api/user/3', {foo: 'bar'})
+async function putMockApi(url, payload) {
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(payload)
+  });
+
+  const result = response.json();
+
+  return result;
+}
+
+async function deleteMockApi(url) {
+  const response = await fetch(url, {
+    method: 'DELETE',
+  });
+
+  const result = response.json();
+
+  return result;
+}
+
 document.getElementById('formSignUp').addEventListener('submit', function(e) {
   e.preventDefault();
 
@@ -35,7 +60,6 @@ document.getElementById('formSignUp').addEventListener('submit', function(e) {
       avatar: 'https://cc.itp.ac.id/wp-content/uploads/2018/10/User-Administrator-Blue-icon.png'
     })
       .then((result) => {
-        console.log(result);
         drawUserList();
 
         document.getElementById('username').value = '';
@@ -46,14 +70,31 @@ document.getElementById('formSignUp').addEventListener('submit', function(e) {
 
 function drawUserList() {
   getMockApi(`${url}/user`)
-    .then((result) => {
+    .then(function (result) {
       const listUser = document.getElementById('listUser');
       listUser.innerHTML = '';
-      console.log(result)
       result.forEach((user) => {
         listUser.innerHTML += 
-          `<li>${user.username} - ${user.password}</li>`
-      })
+          `<li>${user.username} - ${user.password} - <a href="#" class="deleteUser" data-id=${user.id}>Delete</a></li>`
+      });
+
+      const deleteUserElements = document.getElementsByClassName('deleteUser');
+      const deleteUserElementsArray = Array.from(deleteUserElements);
+
+      deleteUserElementsArray.forEach(function(element) {
+        element.addEventListener('click', function(e) {
+          e.preventDefault();
+
+          const id = e.target.dataset.id;
+
+          deleteMockApi(`${url}/user/${id}`)
+            .then(function(result) {
+              console.log(result);
+              drawUserList();
+            });
+
+        });
+      });
     });
 }
 
