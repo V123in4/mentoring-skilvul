27a555defc9cc1f721ece289ff3dc60e644878f1
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2022-10-17 20:04:09 +0700

    refactor pake function

--- Group-Project/script.js
+++ Group-Project/script.js
@@ -1,5 +1,27 @@
 const url = 'https://634808cc0484786c6e90070a.mockapi.io';
 
+async function getMockApi(url) {
+  const response = await fetch(url);
+
+  const result = response.json();
+
+  return result;
+}
+
+async function postMockApi(url, payload) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(payload)
+  });
+
+  const result = response.json();
+
+  return result;
+}
+
 document.getElementById('formSignUp').addEventListener('submit', function(e) {
   e.preventDefault();
 
@@ -7,29 +29,32 @@ document.getElementById('formSignUp').addEventListener('submit', function(e) {
   const password = document.getElementById('password').value;
 
   if (username && password) {
-    console.log(username)
-    fetch(`${url}/user`, {
-      method: 'POST',
-      body: {
-        username,
-        password
-      }
+    postMockApi(`${url}/user`, {
+      username,
+      password,
+      avatar: 'https://cc.itp.ac.id/wp-content/uploads/2018/10/User-Administrator-Blue-icon.png'
     })
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-    })
+      .then((result) => {
+        console.log(result);
+        drawUserList();
+
+        document.getElementById('username').value = '';
+        document.getElementById('password').value = '';
+      })
   }
 });
 
-fetch(`${url}/user`)
-  .then((response) => response.json())
-  .then((result) => {
-    const listUser = document.getElementById('listUser');
-    listUser.innerHTML = '';
-    console.log(result)
-    result.forEach((user) => {
-      listUser.innerHTML += 
-        `<li>${user.username} - ${user.password}</li>`
-    })
-  })
+function drawUserList() {
+  getMockApi(`${url}/user`)
+    .then((result) => {
+      const listUser = document.getElementById('listUser');
+      listUser.innerHTML = '';
+      console.log(result)
+      result.forEach((user) => {
+        listUser.innerHTML += 
+          `<li>${user.username} - ${user.password}</li>`
+      })
+    });
+}
+
+drawUserList();
